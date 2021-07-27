6dad56cffabe78cf22d09e3cf90a6c1550d526b2
Satnam Singh <87562777+satnamwork21@users.noreply.github.com>
2021-07-26 19:55:26 -0700

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,11 @@
+
+#d= float(input("enter the distance in km"))
+#t= float(input("enter the time in sec "))
+#s=d/t 
+#print("speed =",s ,"km/h")
+
+u=float(input("enter the value of U:"))
+t= float(input("enter the time in sec:"))
+a=float(input("enter the value of a:"))
+s= u*t+1/2*a*t**2
+print (s)
